ec13948c3c884739df74ac03040c1ab439471c04
KINGBOTs1 <ds767365@gmail.com>
2023-09-27 04:00:13 +0800

    Initial commit

---
+++ sansekai.js
@@ -0,0 +1,182 @@
+const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require("@adiwajshing/baileys");
+const fs = require("fs");
+const util = require("util");
+const chalk = require("chalk");
+const { Configuration, OpenAIApi } = require("openai");
+const dotenv = require('dotenv');
+const axios = require("axios");
+dotenv.config();
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+module.exports = sansekai = async (client, m, chatUpdate, store) => {
+  try {
+    var body =
+      m.mtype === "conversation"
+        ? m.message.conversation
+        : m.mtype == "imageMessage"
+        ? m.message.imageMessage.caption
+        : m.mtype == "videoMessage"
+        ? m.message.videoMessage.caption
+        : m.mtype == "extendedTextMessage"
+        ? m.message.extendedTextMessage.text
+        : m.mtype == "buttonsResponseMessage"
+        ? m.message.buttonsResponseMessage.selectedButtonId
+        : m.mtype == "listResponseMessage"
+        ? m.message.listResponseMessage.singleSelectReply.selectedRowId
+        : m.mtype == "templateButtonReplyMessage"
+        ? m.message.templateButtonReplyMessage.selectedId
+        : m.mtype === "messageContextInfo"
+        ? m.message.buttonsResponseMessage?.selectedButtonId || m.message.listResponseMessage?.singleSelectReply.selectedRowId || m.text
+        : "";
+    var budy = typeof m.text == "string" ? m.text : "";
+    // var prefix = /^[\\/!#.]/gi.test(body) ? body.match(/^[\\/!#.]/gi) : "/"
+    var prefix = /^[\\/!#.]/gi.test(body) ? body.match(/^[\\/!#.]/gi) : "/";
+    const isCmd2 = body.startsWith(prefix);
+    const command = body.replace(prefix, "").trim().split(/ +/).shift().toLowerCase();
+    const args = body.trim().split(/ +/).slice(1);
+    const pushname = m.pushName || "No Name";
+    const botNumber = await client.decodeJid(client.user.id);
+    const itsMe = m.sender == botNumber ? true : false;
+    let text = (q = args.join(" "));
+    const arg = budy.trim().substring(budy.indexOf(" ") + 1);
+    const arg1 = arg.trim().substring(arg.indexOf(" ") + 1);
+
+    const from = m.chat;
+    const reply = m.reply;
+    const sender = m.sender;
+    const mek = chatUpdate.messages[0];
+
+    const color = (text, color) => {
+      return !color ? chalk.green(text) : chalk.keyword(color)(text);
+    };
+
+    // Group
+    const groupMetadata = m.isGroup ? await client.groupMetadata(m.chat).catch((e) => {}) : "";
+    const groupName = m.isGroup ? groupMetadata.subject : "";
+
+    // Push Message To Console
+    let argsLog = budy.length > 30 ? `${q.substring(0, 30)}...` : budy;
+
+    if (isCmd2 && !m.isGroup) {
+      console.log(chalk.black(chalk.bgWhite("[ LOGS ]")), color(argsLog, "turquoise"), chalk.magenta("From"), chalk.green(pushname), chalk.yellow(`[ ${m.sender.replace("@s.whatsapp.net", "")} ]`));
+    } else if (isCmd2 && m.isGroup) {
+      console.log(
+        chalk.black(chalk.bgWhite("[ LOGS ]")),
+        color(argsLog, "turquoise"),
+        chalk.magenta("From"),
+        chalk.green(pushname),
+        chalk.yellow(`[ ${m.sender.replace("@s.whatsapp.net", "")} ]`),
+        chalk.blueBright("IN"),
+        chalk.green(groupName)
+      );
+    }
+
+    if (isCmd2) {
+      switch (command) {
+        case "help": case "menu": case "start": case "info":
+          m.reply(`*Whatsapp Bot OpenAI*
+            
+*(ChatGPT)*
+Cmd: ${prefix}ai 
+Tanyakan apa saja kepada AI. 
+
+*(DALL-E)*
+Cmd: ${prefix}img
+Membuat gambar dari teks
+
+*(Source Code Bot)*
+Cmd: ${prefix}sc
+Menampilkan source code bot yang dipakai`)
+          break;
+        case "ai": case "openai": case "chatgpt": case "ask":
+          try {
+            // tidak perlu diisi apikeynya disini, karena sudah diisi di file key.json
+            if (!text) return reply(`Chat dengan AI.\n\nContoh:\n${prefix}${command} Apa itu resesi`);
+
+            // Construct the request payload
+            const payload = {
+              message: text,
+              userId: m.sender.replace("@s.whatsapp.net", ""),
+            };
+
+            // Make a POST request to the external API using fetch
+            const response = await fetch("https://chat.dicodingbot.site/chat", {
+              method: "POST",
+              body: JSON.stringify(payload),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            });
+
+            // Check if the response is successful
+            if (response.ok) {
+              const responseData = await response.json();
+              m.reply(responseData.response);
+            } else {
+              // Handle the case where the HTTP request is not successful
+              console.log("HTTP request failed:", response.status, response.statusText);
+              m.reply("Maaf, sepertinya ada yang error saat mengirim permintaan.");
+            }
+            } catch (error) {
+              // Handle other errors here
+              console.error("Error:", error);
+              m.reply("Maaf, sepertinya ada yang error: " + error.message);
+            }
+            break;
+        case "img": case "ai-img": case "image": case "images": case "dall-e": case "dalle":
+          try {
+            // tidak perlu diisi apikeynya disini, karena sudah diisi di file key.json
+            if (OPENAI_API_KEY === "ISI_APIKEY_OPENAI_DISINI") return reply("Apikey belum diisi\n\nSilahkan isi terlebih dahulu apikeynya di file key.json\n\nApikeynya bisa dibuat di website: https://beta.openai.com/account/api-keys");
+            if (!text) return reply(`Membuat gambar dari AI.\n\nContoh:\n${prefix}${command} Wooden house on snow mountain`);
+            const configuration = new Configuration({
+              apiKey: OPENAI_API_KEY,
+            });
+            const openai = new OpenAIApi(configuration);
+            const response = await openai.createImage({
+              prompt: text,
+              n: 1,
+              size: "512x512", // you can change the size of the image here
+            });
+            //console.log(response.data.data[0].url) // see the response
+            client.sendImage(from, response.data.data[0].url, text, mek);
+            } catch (error) {
+          if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+            console.log(`${error.response.status}\n\n${error.response.data}`);
+          } else {
+            console.log(error);
+            m.reply("Maaf, sepertinya ada yang error :"+ error.message);
+          }
+        }
+          break;
+          case "sc": case "script": case "scbot":
+           m.reply("Bot ini menggunakan script dari https://github.com/Sansekai/Wa-OpenAI");
+          break
+        default: {
+          if (isCmd2 && budy.toLowerCase() != undefined) {
+            if (m.chat.endsWith("broadcast")) return;
+            if (m.isBaileys) return;
+            if (!budy.toLowerCase()) return;
+            if (argsLog || (isCmd2 && !m.isGroup)) {
+              // client.sendReadReceipt(m.chat, m.sender, [m.key.id])
+              console.log(chalk.black(chalk.bgRed("[ ERROR ]")), color("command", "turquoise"), color(`${prefix}${command}`, "turquoise"), color("tidak tersedia", "turquoise"));
+            } else if (argsLog || (isCmd2 && m.isGroup)) {
+              // client.sendReadReceipt(m.chat, m.sender, [m.key.id])
+              console.log(chalk.black(chalk.bgRed("[ ERROR ]")), color("command", "turquoise"), color(`${prefix}${command}`, "turquoise"), color("tidak tersedia", "turquoise"));
+            }
+          }
+        }
+      }
+    }
+  } catch (err) {
+    m.reply(util.format(err));
+  }
+};
+
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log(chalk.redBright(`Update ${__filename}`));
+  delete require.cache[file];
+  require(file);
+});
